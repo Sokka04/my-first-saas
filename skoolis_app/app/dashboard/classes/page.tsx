211bdd625ba105198c5f1db987a1267fec490ae6
Sokka04 <Saramba.yoh4nn@outlook.com>
2026-07-18 22:29:33 +0000

    style(classes): fix footer being cut off by printer physical margins

--- skoolis_app/app/dashboard/classes/page.tsx
+++ skoolis_app/app/dashboard/classes/page.tsx
@@ -674,7 +674,7 @@ export default function ClassesPage() {
                         </div>
 
                         {/* Pied de page global (Fixé en bas de la feuille) */}
-                        <div className="fixed bottom-0 left-0 right-0 w-full pt-2 border-t border-black flex justify-between items-center text-xs text-gray-500 bg-white px-8" style={{ fontFamily: "'Times New Roman', Times, serif" }}>
+                        <div className="fixed bottom-[8mm] left-[8mm] right-[8mm] pt-2 border-t border-black flex justify-between items-center text-xs text-gray-500 bg-white px-2" style={{ fontFamily: "'Times New Roman', Times, serif" }}>
                             <div className="w-1/3"></div>
                             <p className="m-0 text-xs text-black w-1/3 text-center mb-1">
                                 <span className="italic font-normal">Propulsé par</span> <span className="font-bold">Skoolis</span>
